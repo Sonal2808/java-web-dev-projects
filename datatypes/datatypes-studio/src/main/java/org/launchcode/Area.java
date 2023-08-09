3d489b1924b19a0038a0665f0adda7e8ad69854e
Sonal2808 <135756443+Sonal2808@users.noreply.github.com>
2023-08-09 13:32:40 -0500

    git commit "Area "

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -1,20 +1,31 @@
 package org.launchcode;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Area {
-public static void main (String[]args){
-    double A;
-    double r;
-   // double pi=3.14;
-    Scanner input;
-    input = new Scanner(System.in);
-    System.out.println("Enter the radius: ");
-    r= input.nextDouble();
-    input.close();
-    A = Circle.getArea(r);
-     System.out.println("area of circle:"+ A);
+    public static void main(String[] args) {
+        double A;
+        double r;
+        Scanner input = new Scanner(System.in);
 
-}
+        do {
+            try {
+                System.out.println("Enter the radius: ");
+                r = input.nextDouble();
 
+                if (r <= 0) {
+                    System.out.println("Radius must be a positive value.");
+                } else {
+                    A = 3.14 * r * r;
+                    System.out.println("Area of circle: " + A);
+                    break;  // Exit the loop if valid input and calculation are done
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input. Please enter a numeric value.");
+                input.nextLine();  // Clear the invalid input from the scanner
+            }
+        } while (true);
 
+        input.close();
+    }
 }
