a2c9112ce954c55ef356af0a21a8676626b9c7f9
Sonal2808 <135756443+Sonal2808@users.noreply.github.com>
2023-08-23 11:31:41 -0500

    commit

--- classes-part-one/exercises/src/main/java/Student.java
+++ classes-part-one/exercises/src/main/java/Student.java
@@ -1,8 +1,45 @@
-public class Student {
+public class Student{
         private String name;
         private int studentId;
-        private int numberOfCredits = 0;
-        private double gpa = 0.0;
+        private int numberOfCredits;
+        private double gpa;
+
+        public Student(String name, int studentId) {
+                this.name = name;
+                this.studentId = studentId;
+        }
+
+        public void setName(String name) {
+                this.name = name;
+        }
+
+        public void setStudentId(int studentId) {
+                this.studentId = studentId;
+        }
+
+        public void setNumberOfCredits(int numberOfCredits) {
+                this.numberOfCredits = numberOfCredits;
+        }
+
+        public void setGpa(double gpa) {
+                this.gpa = gpa;
+        }
+
+        public String getName() {
+                return name;
+        }
+
+        public int getStudentId() {
+                return studentId;
+        }
+
+        public int getNumberOfCredits() {
+                return numberOfCredits;
+        }
+
+        public double getGpa() {
+                return gpa;
+        }
 
         // Drop your getters and setters below for the Student class.
         // To instantiate the Student class, add your code to the main in the file, SchoolPractice.
--- classes-part-one/exercises/src/main/java/StudentPractice.java
+++ classes-part-one/exercises/src/main/java/StudentPractice.java
@@ -1,5 +1,17 @@
 public class StudentPractice {
     public static void main(String[] args){
-        //insantiate your Student class below
+        //instantiate your Student class below
+        Student student = new Student("Salley", 12345);
+        student.setName("Sonal");
+        student.setStudentId(6789);
+        student.setNumberOfCredits(1);
+        student.setGpa(4.0);
+
+        System.out.println("Student Name: " + student.getName());
+        System.out.println("Student ID: " + student.getStudentId());
+        System.out.println("Number of Credits: " + student.getNumberOfCredits());
+        System.out.println("GPA: " + student.getGpa());
     }
+
+
 }
